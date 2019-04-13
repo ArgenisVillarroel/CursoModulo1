a1e31d77ac0d6868b42b5786cac341919da35d46
Argens Villarroel <smitterargenis@gmail.com>
2019-04-13 00:06:43 -0400

    Cambios

--- AccountManager.Data/Models/DTO/AccountDTO.cs
+++ AccountManager.Data/Models/DTO/AccountDTO.cs
@@ -8,14 +8,8 @@ namespace AccountManager.Data.Models.DTO
     public class AccountDTO
     {
         public int Id { get; set; }
-
- 
         public string Code { get; set; }
-
- 
         public string Name { get; set; }
-
-
         public ViewModelDTO<int> AccountType { get; set; }
     }
 }
